a62c933ad876af8ca608c53a477349c9ff57462e
Pedro Silva <pedro.rls93@gmail.com>
2018-05-21 22:07:34 -0300

    first reducer created to handle action and payload

---
+++ app/reducers/currencies.js
@@ -0,0 +1,28 @@
+import { CHANGE_CURRENCY_AMOUNT, SWAP_CURRENCY } from '../actions/currencies';
+
+const initalState = {
+    baseCurrency: 'BRL',
+    quoteCurrency: 'USD',
+    amount: 100,
+    conversions: {},
+};
+
+const reducer = (state = initalState, action) => {
+    switch(action.type){
+        case CHANGE_CURRENCY_AMOUNT:
+            return {
+                ...state, 
+                mount: action.amount || 0,
+            }
+        case SWAP_CURRENCY:
+            return {
+                ...state,
+                baseCurrency: state.quoteCurrency,
+                quoteCurrency: state.baseCurrency,
+            }
+        default:
+            return state;
+    }
+};
+
+export default reducer
